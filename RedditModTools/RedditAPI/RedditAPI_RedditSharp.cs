e027f990f727cd071993db38dd512013275df86e
Windex007 <srnorton@ucalgary.ca>
2014-12-16 14:27:10 -0700

    Added the initial project files.

---
+++ RedditModTools/RedditAPI/RedditAPI_RedditSharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp;
+
+namespace RedditAPI
+{
+    public class RedditAPI_RedditSharp
+    {
+        Reddit rBaseSrv;
+        bool isLoggedIn;
+
+
+        public RedditAPI_RedditSharp()
+        { 
+            rBaseSrv = null;
+            isLoggedIn = false;
+        }
+
+        public bool login(string username, string password) 
+        {
+            rBaseSrv = new Reddit(username, password);
+            isLoggedIn = (rBaseSrv.User != null);
+            return isLoggedIn;
+        }
+    }
+}
---
+++ RedditModTools/RedditModTools/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using RedditSharp;
+using RedditSharp.Things;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RedditModTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Reddit reddit;
+        Listing<VotableThing> reports;
+        Listing<Post> unmoderatedLinks;
+
+        public string subreddit;
+        public bool isLoggedIn;
+
+        Subreddit sub;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initialize();
+        }
+        public void initialize()
+        {
+            
+            subreddit = "thatHappened";
+
+            isLoggedIn = false;
+
+            reports = null;
+            
+        }
+        public void populateUnmodded()
+        {
+            textArea.Text = "";
+            
+
+            foreach (Post post in unmoderatedLinks)
+                textArea.Text += post.Title + "\n";
+        }
+
+
+        private void login_loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            reddit = new Reddit(login_usernameTextbox.Text, login_passwordTextbox.Text);
+
+            isLoggedIn = (reddit.User != null);
+
+            sub = reddit.GetSubreddit(subreddit);
+
+            MessageBox.Show("loggedin");
+        }
+
+        private void popButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLoggedIn)
+            {
+                Listing<Post> newStuff = sub.GetUnmoderatedLinks();
+                if (newStuff != null)
+                    unmoderatedLinks = newStuff;
+
+                populateUnmodded();
+            }
+        }
+    }
+}
